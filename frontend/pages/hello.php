cfef8f0d1b326b79c4e2910565b9fc0702227145
frank bahirwa <frankbahirwa@gmail.com>
2024-09-27 09:31:39 +0200

    updating the frontend to look so nice

--- frontend/pages/hello.php
+++ frontend/pages/hello.php
@@ -3,41 +3,34 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=
-, initial-scale=1.0">
+<meta name="viewport" content="width=initial-scale=1.0">
 <title>Document</title>
-<link rel="stylesheet" href="hello.css">
+<link rel="stylesheet" href="../css/hello.css">
 </head>
 <body>
+<div class="menu">
+<form>
 
-    <div class="menu">
-    <form>
-      <div class="icons">
-        
-        <label for="username">  username:</label><br>
-   <input type="username" id="username"name =" username"><br><br>
-   
-<label for="idnumber"> id number:</label><br>
-<input type="number" id="number"name="nuber"><br><br>
+<p id="header">Create Your Account</p> <br>
 
-<label for="email">  email:</label><br>
-<input type="email" id="email"name="email"><br><br>
-
-<label for="password"> password:</label>
-<input type="password" id="password" name="password"><br><br>
+<div class="icons">     
+<input type="username" id="username"name =" username" placeholder="username"><br><br>
+<input type="number" id="number"name="nuber" placeholder="phone number"><br><br>
+<input type="email" id="email"name="email" placeholder="email"><br><br>
+<input type="password" id="password" name="password" placeholder="password"><br><br>
 </div>
+
 <div class="select">
-<label for="favouratefood">favourate food</label>
 <select name="favouratefood" id="favouratefood">
+<option value="" selected>select your favourite</option>
    <option value="proteins">proteins</option>
    <option value="vegetables">vegetables</option>
    <option value="fruits">fruits</option>
    <option value="white meat">white meat</option>
 </select>
 </div>
-
+<br>
 <button type="submit" class="button">Submit</button>
-
 
 </form>
 </div>
