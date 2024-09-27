62413ec555adbe737683187f2f0d1020d2caaa9d
Annithah <167645879+Annithah@users.noreply.github.com>
2024-09-26 17:49:27 -1200

    up to dste

---
+++ hello.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=
+, initial-scale=1.0">
+<title>Document</title>
+<link rel="stylesheet" href="hello.css">
+</head>
+<body>
+
+    <div class="menu">
+    <form>
+        
+        <label for="username">  username:</label><br>
+   <input type="username" id="username"name =" username"><br><br>
+   
+<label for="idnumber"> id number:</label><br>
+<input type="number" id="number"name="nuber"><br><br>
+
+<label for="email">  email:</label><br>
+<input type="email" id="email"name="email"><br><br>
+
+<label for="password"> password:</label>
+<input type="password" id="password" name="password"><br><br>
+
+<label for="favouratefood">favourate food</label>
+<select name="favouratefood" id="favouratefood">
+   <option value="proteins">proteins</option>
+   <option value="vegetables">vegetables</option>
+   <option value="fruits">fruits</option>
+   <option value="white meat">white meat</option>
+</select>
+
+<button type="submit" class="button">Submit</button>
+
+
+</form>
+</div>
+</body>
+</html>
